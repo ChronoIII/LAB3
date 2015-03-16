1e13a0d8bf499f0ce4184a434e87268fc9354047
ChronoIII <samuel@jfzone.net>
2015-03-15 21:25:32 -0400

    JUnite, buildpath>lib>plugin>org.JUnit

---
+++ src/Bunco/TestBunco.java
@@ -0,0 +1,70 @@
+/******************************************************
+Cours:   LOG121
+Session: H2015
+Groupe: 03
+Projet: Laboratoire #
+Étudiant(e)s: Samuel Laroche
+              
+              
+Chargé de cours : Francis Cardinal
+Chargé de laboratoire : Patrice Boucher
+Nom du fichier: 
+Date créé: 2015-0X-XX
+Date dern. modif. 2015-0X-XX
+*******************************************************
+Historique des modifications
+*******************************************************
+2015-0X-XX Version initiale 
+*******************************************************/
+
+
+package Bunco;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Samuel
+ *
+ */
+public class TestBunco {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
