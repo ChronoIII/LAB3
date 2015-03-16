8e7f21ac8a07ca772dc9c7abe9984023f13cf38b
Kuurin (Amélie) <tuong_vi06@hotmail.com>
2015-03-15 20:51:32 -0400

    Ouais euh j'ai mis un peu la logique dedans mais je comprend tjs pas a 100% le code alors c pas encore la vraie version x)

--- src/bunco/BuncoStrategie.java
+++ src/bunco/BuncoStrategie.java
@@ -6,6 +6,12 @@ import frameworkJeuDeDes.Jeu;
 public class BuncoStrategie implements IStrategie {
 	public static final int NB_TOURS = 6;
 	public static final int NB_DES = 3;
+	
+	private int tour = 1;
+	private int similaire;
+	private boolean pointNonTour;
+	private boolean bunco;
+	private int somme=0;
 
 	@Override
 	public String calculerLeVainqueur(Jeu jeu) {
@@ -18,7 +24,69 @@ public class BuncoStrategie implements IStrategie {
 		// TODO Auto-generated method stub
 		//utiliser
 		//(de1.compareTo(de2)==0) ou quelque chose du genre pour bunco
+
+		jeu.getListDes().createIterator();
+
+		
+
+		do{
+			similaire =0;
+			pointNonTour = false;
+			bunco = true;
+
+
+			for (int i =0; i<NB_DES; i++){
+				jeu.getListDes().getDe(i).rouler();}
+
+			if (Integer.parseInt(jeu.getListDes().getDe(0).getFaceObtenue())==tour){
+
+				similaire++;
+
+			}
+			if (Integer.parseInt(jeu.getListDes().getDe(1).getFaceObtenue())==tour){
+
+				similaire++;
+
+			}
+			if (Integer.parseInt(jeu.getListDes().getDe(2).getFaceObtenue())==tour){
+
+				similaire++;
+
+			}
+			if (similaire == 3){
+
+				bunco = true;
+
+			}
+
+			if (Integer.parseInt(jeu.getListDes().getDe(0).getFaceObtenue())!=tour&&Integer.parseInt(jeu.getListDes().getDe(0).getFaceObtenue()) == Integer.parseInt(jeu.getListDes().getDe(1).getFaceObtenue())&&Integer.parseInt(jeu.getListDes().getDe(1).getFaceObtenue())==Integer.parseInt(jeu.getListDes().getDe(2).getFaceObtenue())){
+				
+				pointNonTour=true;
+
+			}
+			
+			tour++;
+			
+		}while(tour <=6 && similaire !=0 &&!bunco);
+		
+		//calculer point
+		
+		if (similaire == 1){
+			somme = somme +1;	
+		}
+		else if (similaire == 2){
+			somme = somme +2;	
+		}
+		else if (bunco){
+			somme = somme +21;	
+		}else if (pointNonTour){
+			somme = somme +5;	
+		}
+		
+		//joueur.setPointage(somme);
+
 	}
+
 
 	@Override
 	public int getNombreTours() {
